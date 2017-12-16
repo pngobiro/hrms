2fbce2c854d90dbf904f20ac2b7b59e3473095c5
pngobiro <pngobiro@gmail.com>
2017-12-16 15:48:03 +0300

    added designation relationship

--- employees/models.py
+++ employees/models.py
@@ -3,16 +3,38 @@ from django.db import models
 # Create your models here.
 
 class Employee(models.Model):
-    GENDER = ((1, 'Male'),(2, 'Female'),(3, 'Transgender'),)
     
-    first_name = models.CharField(max_length=20)
-    middle_name = models.CharField(max_length=20)
-    last_name = models.CharField(max_length=20)
-    id_number = models.CharField(max_length=10)
-    pj_number = models.IntegerField()
-    nhif_number = models.CharField(max_length=20)
-    nssf_number = models.CharField(max_length=20)
-    date_of_birth = models.DateField()
-    gender = models.IntegerField(choices=GENDER)
-    date_of_retirement = models.DateField(blank=True,null=True)
+        GENDER = ((1, 'Male'),(2, 'Female'),(3, 'Transgender'),)
+        first_name = models.CharField(max_length=20,help_text="Employee First Name")
+        middle_name = models.CharField(max_length=20,help_text="Employee Middle Name")
+        last_name = models.CharField(max_length=20,help_text="Employee Surname Name")
+        id_number = models.CharField(max_length=10,help_text="Passport or National Identification Number")
+        pj_number = models.IntegerField(unique= True)
+        nhif_number = models.CharField(max_length=20,unique= True)
+        nssf_number = models.CharField(max_length=20,unique= True)
+        date_of_birth = models.DateField(verbose_name = "Date of Birth")
+        gender = models.IntegerField(choices=GENDER)
+        designation = models.ManyToManyField("Designation",related_name="employees")
+        date_of_retirement = models.DateField(blank=True,null=True)
     
+        def __str__(self):
+            return (" ".join([self.first_name, self.middle_name, self.last_name]))
+        
+        class Meta:
+           ordering = ('pj_number',)
+        
+    
+class Designation(models.Model):
+   
+        
+        name = models.CharField(max_length=20,help_text="Designation Name",unique= True)
+        
+        def __str__(self):
+              return self.name
+          
+        class Meta:
+            ordering = ('name',)
+          
+
+    
+              
