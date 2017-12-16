a59ccaf15e0d9c8bdf63f2f88bf70b17a47408f7
pngobiro <pngobiro@gmail.com>
2017-12-16 17:31:12 +0300

    admin customasition

--- employees/admin.py
+++ employees/admin.py
@@ -1,7 +1,20 @@
 from django.contrib import admin
-from .models import Employee,Designation
+from .models import Employee,Designation,EmployeeDesignations
+from django.utils import timezone
 
+class EmployeeAdmin(admin.ModelAdmin):
+    
+            list_display =("full_name","pj_number","gender","age")
+            
+            def full_name(self,obj):
+                return (" ".join([obj.first_name, obj.middle_name, obj.last_name]))
+            
+            def age(self,obj):
+                return  timezone.now().year - obj.date_of_birth.year
+            
+            list_filter = ("gender",)
 
 # Register your models here.
-admin.site.register(Employee)
+admin.site.register(Employee,EmployeeAdmin)
 admin.site.register(Designation)
+admin.site.register(EmployeeDesignations)
--- employees/models.py
+++ employees/models.py
@@ -1,9 +1,9 @@
 from django.db import models
 
+
 # Create your models here.
 
 class Employee(models.Model):
-    
         GENDER = ((1, 'Male'),(2, 'Female'),(3, 'Transgender'),)
         first_name = models.CharField(max_length=20,help_text="Employee First Name")
         middle_name = models.CharField(max_length=20,help_text="Employee Middle Name")
@@ -14,27 +14,47 @@ class Employee(models.Model):
         nssf_number = models.CharField(max_length=20,unique= True)
         date_of_birth = models.DateField(verbose_name = "Date of Birth")
         gender = models.IntegerField(choices=GENDER)
-        designation = models.ManyToManyField("Designation",related_name="employees")
         date_of_retirement = models.DateField(blank=True,null=True)
-    
+
         def __str__(self):
             return (" ".join([self.first_name, self.middle_name, self.last_name]))
+
+      
         
+
+            
+        def current_designation(self):
+            self.employeedesignation.last
+
         class Meta:
            ordering = ('pj_number',)
-        
-    
+           
+           
+
+
+
+
+
 class Designation(models.Model):
-   
-        
         name = models.CharField(max_length=20,help_text="Designation Name",unique= True)
-        
+
         def __str__(self):
               return self.name
-          
+
         class Meta:
             ordering = ('name',)
-          
 
-    
-              
+class EmployeeDesignations(models.Model):
+         employee = models.ForeignKey(Employee, on_delete=models.CASCADE)
+         designation = models.ForeignKey(Designation, on_delete=models.CASCADE)
+         date_of_apppointment = models.DateField()
+         remarks = models.CharField(max_length=250,blank=True,null=True)
+         
+         def __str__(self):
+                return "{} -{}".format(self.employee,self.designation)
+            
+
+
+
+
+
